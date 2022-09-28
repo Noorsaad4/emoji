96e7c3be52dae05b122ad9f229563bd53459a27c
Noorsaad4 <114570156+Noorsaad4@users.noreply.github.com>
2022-09-28 13:38:26 +0300

    Initial commit

---
+++ src/emoji/Main.java
@@ -0,0 +1,14 @@
+package emoji;
+
+public class Main {
+
+	public static void main(String[] args) {
+	string yuzIfadesi="üzgün";
+	if(yuzIfadesi=="mutlu") {
+      System.out.println(":)");
+	}
+	
+    if(üzgün) {
+	 System.out.println(":(");
+}
+
